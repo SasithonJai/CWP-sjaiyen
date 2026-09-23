29c8257ab35388d46a2dc0ed36463159fc19ec73
SasithonJai <66100397@kmitl.ac.th>
2026-09-23 11:16:33 +0700

    Create play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+new_numbers = []
+
+for number in numbers:
+    new_numbers.append(number + 2)
+
+print("Original array:", numbers)
+print("New array:", new_numbers)
