667b21da9c626a1aec0649daf8311de00a6b5ff1
SasithonJai <66100397@kmitl.ac.th>
2026-09-23 16:39:58 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+first_number = input("Enter the first number:\n")
+first_number = int(first_number)
+
+second_number = input("Enter the second number:\n")
+second_number = int(second_number)
 
 result = first_number * second_number
+
+print(str(first_number) + " x " + str(second_number) + " = " + str(result))
 
 if result > 0:
     print("The result is positive.")
 elif result < 0:
     print("The result is negative.")
 else:
-    print("The result is positive and negative.")
+    print("The result is zero.")
     
