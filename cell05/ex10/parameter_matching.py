b92e18e0355a9b81686f4bf88487a83f435f429f
SasithonJai <66100397@kmitl.ac.th>
2026-09-23 14:12:30 +0700

    Create parameter_matching.py

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.argv = ["parameter_matching.py", "Hello"]
+
+if len(sys.argv) == 2:
+    word = input("What was the parameter? ")
+    if word == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+    print("none")
