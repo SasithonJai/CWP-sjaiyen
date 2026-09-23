42249cee44a738067826502711d7de8df26a90ab
SasithonJai <66100397@kmitl.ac.th>
2026-09-23 10:24:07 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number1 = int(input("Give me the first number: "))
+number2 = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(number1, "+", number2, "=", number1 + number2)
+print(number1, "-", number2, "=", number1 - number2)
+print(number1, "/", number2, "=", number1 // number2)
+print(number1, "*", number2, "=", number1 * number2)
